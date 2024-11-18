7befb8a6d3e8106101b92f0187298dd5d6b0b123
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-18 13:49:45 -0300

    ajustes

--- src/components/submenu.tsx
+++ src/components/submenu.tsx
@@ -15,7 +15,7 @@ const styleSubmenu = {
 export const Submenu: React.FC<SubmenuProps> = ({op1, op2, op3, op4}) => {
     return(
         <div className="bg-gray-100 h-full w-[250px] fixed font-robFont text-large border-gray-300 border-[0.5px]">
-            <h3 className="mt-24 text-[18px] text-black bg-gray-200 p-2 pl-4">Menu principal</h3>
+            <h3 className="mt-24 text-[18px] text-black bg-gray-200 p-2 pl-4">MENU PRINCIPAL</h3>
             <div className="flex flex-col p-6">
                 <Link href={ROUTES.home} className={styleSubmenu.link}>{op1}</Link>
                 <Link href={ROUTES.users} className={styleSubmenu.link}>{op2}</Link>
