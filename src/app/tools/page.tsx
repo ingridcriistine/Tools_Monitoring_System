6c5268748f487d07d1219fc7ca2a609897b2f9ad
milena579 <milecalegari@gmail.com>
2024-11-19 15:39:30 -0300

    users finalizado

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
+import imgLupa from "@/app/assets/lupa.png"
 
 export default function Tools() {
   return (
     <div className="bg-white text-black flex flex-col min-h-screen p-8 ml-[250px] pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div className="flex flex-row justify-between mt-16 pb-4">
             <h3 className="text-[22px] font-bold">Ferramentas</h3>
-            <button className="bg-green-200 p-2">Adicionar ferramenta</button>
+            <button className="bg-green-200 p-2 pr-4 pl-4 rounded-lg">Adicionar ferramenta</button>
         </div>
         <hr/>
-        <input></input>
+        <div className="flex flex-row mt-8 w-full justify-end">
+          <input type="search" placeholder="Pesquisar ferramenta" className="w-[450px] border-b-2 pl-2 focus:outline-none"/>
+          <Image src={imgLupa} alt="" className="w-3 h-3 m-2 relative right-8"/>
+        </div>
     </div>
   );
 }
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -3,39 +3,41 @@ import { useState } from "react"
 import Image from "next/image"
 import deletar from "@/app/assets/delete.png"
 
-interface linha {
-    EDV : string, 
-    RFID : string,
-    name : string
-}
-
 interface dadoLinhasTable {
     tituloUm: string,
     tituloDois: string
-    dadoLinha: {EDV : string,  RFID : string, name : string}[] | undefined
+    tituloTres: string
+    dadoLinha: {EDV : string,  RFID : string, name : string}[] 
 }
 
-const Tabela: React.FC<dadoLinhasTable> = ({tituloUm, tituloDois,  dadoLinha}) => {{
+const Tabela: React.FC<dadoLinhasTable> = ({tituloUm, tituloDois, tituloTres, dadoLinha}) => {{
+    console.log(dadoLinha)
     return (
         <> 
-         <table>
-            <tr>
-                <th>{tituloUm}</th>
-                <th>{tituloDois}</th>
-                <th>Deletar</th>
-            </tr>
-                {dadoLinha?.map((item, index) => 
-                <tr>
-                    <td>{item.EDV}</td>
-                    <td>{item.RFID}</td>
-                    <td>{item.name}</td>
-                    <td><Image src={deletar} alt=""></Image></td>
+         <table className="flex w-full flex-col">
+            <thead className="w-full items-center flex ">
+                <tr className="flex w-full items-center">
+                    <th className="justify-center flex w-[25%] p-5">{tituloUm}</th>
+                    <th className="justify-center flex w-[25%] p-5">{tituloDois}</th>
+                    <th className="justify-center flex w-[25%] p-5">{tituloTres}</th>
+                    <th className="justify-center flex w-[25%] p-5 ">Deletar</th>
                 </tr>
+            </thead>
+            <tbody className="w-full items-center">
+                {dadoLinha?.map((item, index) => (
+                    <tr key={index} className="flex w-full items-center	">
+                        <td className="justify-center flex w-[25%] border-b-2 p-2">{item.EDV}</td>
+                        <td className="justify-center flex w-[25%] border-b-2 p-2">{item.RFID}</td>
+                        <td className="justify-center flex w-[25%] border-b-2 p-2">{item.name}</td>
+                        <td className="justify-center flex w-[25%] border-b-2 p-2.5 cursor-pointer"><Image src={deletar} alt="" width={20}></Image></td>
+                    </tr>
+                )   
                 )}
+            </tbody>
         </table>
         </>
 
     );
 }}
 
-export default Tabela
+export default Tabela;
