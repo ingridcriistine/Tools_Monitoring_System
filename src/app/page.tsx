faeee304943bebd2fa365a97018f0b0c0fa5943a
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-25 15:26:52 -0300

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import imgTools from "@/app/assets/tools.jpg";
 
 export default function Home() {
   return (
-    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ml-[200px]">
         <div className="w-full flex h-52 items-top">
-          <Image src={imgTools} alt=""  className="flex w-full h-full object-cover object-top"/>
+          <Image src={imgTools} alt=""  className="flex w-full h-full object-cover object-top  bg-white rounded-3xl align-center shadow-md shadow-slate-400"/>
         </div>
         <div className="flex flex-row flex-wrap">
           <CardTool imagem={1} title={"Martelo"} status={"true"}/>
