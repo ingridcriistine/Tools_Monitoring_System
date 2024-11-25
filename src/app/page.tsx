284f5b3cb3e2e222e675e8dd7b9c854380615b26
milena579 <milecalegari@gmail.com>
2024-11-25 15:06:39 -0300

    "banner"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ml-[200px]">
-        <Image src={imgTools} alt="" width={150} height={100} className="w-full"/>
+        <div className="w-full flex h-52 items-top">
+          <Image src={imgTools} alt=""  className="flex w-full h-full object-cover object-top"/>
+        </div>
         <div className="flex flex-row flex-wrap">
           <CardTool imagem={1} title={"Martelo"} status={"true"}/>
           <CardTool imagem={2} title={"Alicate"} status={"true"}/>
