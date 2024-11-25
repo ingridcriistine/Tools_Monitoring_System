ab0fd06f2667901632322596a659a405536f0133
milena579 <milecalegari@gmail.com>
2024-11-25 14:26:35 -0300

    add img tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { CardTool } from "@/components/cardTool";
 import Image from "next/image";
-import imgTools from "@/app/assets/bosch.png";
+import imgTools from "@/app/assets/tools.jpg";
 
 export default function Home() {
   return (
     <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ml-[200px]">
-        <Image src={imgTools} alt="" width={10} height={10}/>
+        <Image src={imgTools} alt="" width={150} height={100} className="w-full"/>
         <div className="flex flex-row flex-wrap">
           <CardTool imagem={1} title={"Martelo"} status={"true"}/>
           <CardTool imagem={2} title={"Alicate"} status={"true"}/>
